CherishOS/android_packages_apps_Settings

Settings: Show Android version as 12L
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionDetailPreferenceController.java
@@ -21,6 +21,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.SystemClock;
 import android.os.UserHandle;
+import android.os.SystemProperties;
 import android.os.UserManager;
 import android.text.TextUtils;
 import android.util.Log;
@@ -36,6 +37,9 @@ import com.android.settingslib.RestrictedLockUtils;
 import com.android.settingslib.RestrictedLockUtilsInternal;
 
 public class FirmwareVersionDetailPreferenceController extends BasePreferenceController {
+
+    @VisibleForTesting
+    private static final String CHERISH_PLATFORM_RELEASE_OR_CODENAME = "ro.cherish.platform_release_or_codename";
 
     private static final String TAG = "firmwareDialogCtrl";
     private static final int DELAY_TIMER_MILLIS = 500;
@@ -75,7 +79,8 @@ public class FirmwareVersionDetailPreferenceController extends BasePreferenceCon
 
     @Override
     public CharSequence getSummary() {
-        return Build.VERSION.RELEASE_OR_CODENAME;
+        return SystemProperties.get(CHERISH_PLATFORM_RELEASE_OR_CODENAME, 
+            Build.VERSION.RELEASE_OR_CODENAME);
     }
 
     @Override

src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionPreferenceController.java
@@ -16,12 +16,18 @@
 
 package com.android.settings.deviceinfo.firmwareversion;
 
-import android.content.Context;
 import android.os.Build;
+import android.content.Context;
+import android.os.SystemProperties;
+import androidx.annotation.VisibleForTesting;
 
+import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
 
 public class FirmwareVersionPreferenceController extends BasePreferenceController {
+
+    @VisibleForTesting
+    private static final String CHERISH_PLATFORM_RELEASE_OR_CODENAME = "ro.cherish.platform_release_or_codename";
 
     public FirmwareVersionPreferenceController(Context context, String key) {
         super(context, key);
@@ -34,6 +40,7 @@ public class FirmwareVersionPreferenceController extends BasePreferenceControlle
 
     @Override
     public CharSequence getSummary() {
-        return Build.VERSION.RELEASE_OR_CODENAME;
+        return SystemProperties.get(CHERISH_PLATFORM_RELEASE_OR_CODENAME, 
+            Build.VERSION.RELEASE_OR_CODENAME);
     }
 }

Techy0302/packages_apps_Settings

Signed-off-by: Dmitrii <bankersenator@gmail.com>

src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionDetailPreferenceController.java
@@ -21,6 +21,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.SystemClock;
 import android.os.UserHandle;
+import android.os.SystemProperties;
 import android.os.UserManager;
 import android.text.TextUtils;
 import android.util.Log;
@@ -36,6 +37,9 @@ import com.android.settingslib.RestrictedLockUtils;
 import com.android.settingslib.RestrictedLockUtilsInternal;
 
 public class FirmwareVersionDetailPreferenceController extends BasePreferenceController {
+
+    @VisibleForTesting
+    private static final String VOLTAGE_PLATFORM_RELEASE_OR_CODENAME = "ro.voltage.platform_release_or_codename";
 
     private static final String TAG = "firmwareDialogCtrl";
     private static final int DELAY_TIMER_MILLIS = 500;
@@ -75,7 +79,8 @@ public class FirmwareVersionDetailPreferenceController extends BasePreferenceCon
 
     @Override
     public CharSequence getSummary() {
-        return Build.VERSION.RELEASE_OR_CODENAME;
+        return SystemProperties.get(VOLTAGE_PLATFORM_RELEASE_OR_CODENAME, 
+            Build.VERSION.RELEASE_OR_CODENAME);
     }
 
     @Override

src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionPreferenceController.java
@@ -16,12 +16,18 @@
 
 package com.android.settings.deviceinfo.firmwareversion;
 
-import android.content.Context;
 import android.os.Build;
+import android.content.Context;
+import android.os.SystemProperties;
+import androidx.annotation.VisibleForTesting;
 
+import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
 
 public class FirmwareVersionPreferenceController extends BasePreferenceController {
+
+    @VisibleForTesting
+    private static final String VOLTAGE_PLATFORM_RELEASE_OR_CODENAME = "ro.voltage.platform_release_or_codename";
 
     public FirmwareVersionPreferenceController(Context context, String key) {
         super(context, key);
@@ -34,6 +40,7 @@ public class FirmwareVersionPreferenceController extends BasePreferenceControlle
 
     @Override
     public CharSequence getSummary() {
-        return Build.VERSION.RELEASE_OR_CODENAME;
+        return SystemProperties.get(VOLTAGE_PLATFORM_RELEASE_OR_CODENAME, 
+            Build.VERSION.RELEASE_OR_CODENAME);
     }
 }